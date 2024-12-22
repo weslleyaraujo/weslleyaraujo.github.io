b2b73424bedaad1eb9fc22b4c00da6028045ccb4
Weslley Araujo <weslleyaraujo20@gmail.com>
2024-12-22 08:57:20 +0100

    remove images

--- src/data/galleries/index.ts
+++
@@ -1,6 +0,0 @@
-// Generated file - do not edit
-import { aiguestoresberrosjussa } from "./2024-aug-aiguestores-berros-jussa";
-import { netherlands } from "./2024-oct-netherlands";
-import { montreal } from "./2024-sep-montreal";
-
-export { aiguestoresberrosjussa, netherlands, montreal };
