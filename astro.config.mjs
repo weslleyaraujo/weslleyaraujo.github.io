25b961ed09fc8086df19a69e7a36b73c4799b932
Weslley Araujo <weslleyaraujo20@gmail.com>
2025-01-01 19:55:14 +0100

    kinda works?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,24 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import mdx from "@astrojs/mdx";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
-// https://astro.build/config
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
   integrations: [
     tailwind(),
-    mdx(),
     sanity({
       projectId: "erw67dz9",
       dataset: "production",
       useCdn: false,
-      studioBasePath: "/studio",
+      ...(isProduction ? {} : { studioBasePath: "/studio" }),
     }),
     react(),
   ],
   site: "https://weslleyaraujo.github.io",
   base: "",
+  output: "static",
   vite: {
     build: {
       rollupOptions: {
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-export const SITE_NAME = "Nice Tat";
+export const SITE_NAME = "Weslley Araujo";
 
-export const DESCRIPTION =
-  "Astro Site Template inspired by Casey Neistat’s ultra minimal personal site. ";
+export const DESCRIPTION = "This is my website";
