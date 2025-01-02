4af71fc188f33b9b6163f600f01ad5b41d3c8577
Weslley Araujo <weslleyaraujo20@gmail.com>
2025-01-02 13:36:40 +0100

    progress

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -7,29 +7,33 @@ interface Props {
 }
 
 export default function Gallery({ images }: Props) {
-  const chunks = images.reduce<(typeof images)[]>((acc, current, index) => {
-    const chunkIndex = Math.floor(index / 4);
-    if (!acc[chunkIndex]) acc[chunkIndex] = [];
-    acc[chunkIndex].push(current);
-    return acc;
-  }, []);
+  const imagesPerRow = 3;
+  const chunkCount = Math.ceil(images.length / imagesPerRow);
+
+  // Distribute images in a round-robin fashion
+  const chunks: (typeof images)[] = Array.from(
+    { length: chunkCount },
+    () => []
+  );
+  images.forEach((image, index) => {
+    const chunkIndex = index % chunkCount; // Cyclically distribute images
+    chunks[chunkIndex].push(image);
+  });
 
   return (
-    <>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 auto-rows-max">
-        {chunks.map((chunk, chunkIndex) => (
-          <div key={chunkIndex} className="grid gap-4 auto-rows-max">
-            {chunk.map((image, index) => (
-              <div key={index}>
-                <Picture
-                  url={imageBuilder.image(image).url()}
-                  dimensions={[800, 1400, 1920]}
-                />
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 auto-rows-max">
+      {chunks.map((chunk, chunkIndex) => (
+        <div key={chunkIndex} className="grid gap-4 auto-rows-max">
+          {chunk.map((image, index) => (
+            <div key={index}>
+              <Picture
+                url={imageBuilder.image(image).url()}
+                dimensions={[800, 1400, 1920]}
+              />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
