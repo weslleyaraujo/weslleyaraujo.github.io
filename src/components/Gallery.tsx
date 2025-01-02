0c0d7132657bd8a674b4cc6831a1a4d369c25194
Weslley Araujo <weslleyaraujo20@gmail.com>
2025-01-02 13:20:30 +0100

    tailwind

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,34 @@
+import type { SanityAssetDocument } from "@sanity/client";
+import { imageBuilder } from "../sanity/lib/url-for-image";
+import Picture from "./Picture";
+
+interface Props {
+  images: SanityAssetDocument[];
+}
+export default function Gallery({ images }: Props) {
+  const chunks = images.reduce<(typeof images)[]>((acc, current, index) => {
+    const chunkIndex = Math.floor(index / 4);
+    if (!acc[chunkIndex]) acc[chunkIndex] = [];
+    acc[chunkIndex].push(current);
+    return acc;
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 auto-rows-max">
+        {chunks.map((chunk, chunkIndex) => (
+          <div key={chunkIndex} className="grid gap-4 auto-rows-max">
+            {chunk.map((image, index) => (
+              <div key={index}>
+                <Picture
+                  url={imageBuilder.image(image).url()}
+                  dimensions={[800, 1400, 1920]}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ src/components/Picture.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+interface Props {
+  url: string;
+  dimensions: number[];
+}
+
+export default function Picture({ url, dimensions }: Props) {
+  return (
+    <picture className="h-auto max-w-full rounded-lg">
+      <source
+        type="image/webp"
+        srcSet={generateSrcSet(url, dimensions, "webp")}
+        sizes="(max-width: 640px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
+
+      <img
+        src={`${url}?w=${dimensions[0]}`}
+        srcSet={generateSrcSet(url, dimensions, "jpg")}
+        sizes="(max-width: 640px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        alt="Responsive image"
+        className="h-auto max-w-full rounded-lg"
+        loading="lazy"
+        decoding="async"
+      />
+    </picture>
+  );
+}
+
+function generateSrcSet(url: string, dimensions: number[], format: string) {
+  return dimensions
+    .map((width) => `${url}?w=${width}&fm=${format} ${width}w`)
+    .join(", ");
+}
